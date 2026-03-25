7ada85a42af9ae7149e175411db9cb1b54cfb94d
Alessandra <131419040+AlessandraBarbosaSalazar@users.noreply.github.com>
2026-03-25 00:29:43 -0600

    Update app.js

--- app.js
+++ app.js
@@ -112,3 +112,10 @@ if ("serviceWorker" in navigator) {
 }
 
 loadSong(index);
+
+
+navigator.mediaSession.metadata = new MediaMetadata({
+  title: song.title,
+  artist: song.artist,
+  artwork: [{ src: song.cover, sizes: "512x512", type: "image/jpeg" }]
+});
